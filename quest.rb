6fe66793e555140b2556c22d4cb4c777be291082
Ivan Necas <necasik@gmail.com>
2013-09-26 11:19:29 +0200

    First version

--- quest.rb
+++ quest.rb
@@ -1,7 +1,27 @@
 #!/usr/bin/env ruby
 
 def parse_data(data)
-  raise NotImplementedError
+  questions = []
+  question = {:answers => []}
+  data.lines.each do |line|
+    line = line.chomp
+    if question[:question].nil?
+      question[:question] = line
+    elsif line.empty?
+    elsif line == '='
+      questions << question
+      question = {:answers => []}
+    else
+      answer = { :answer => line, :correct => false }
+      if line =~ /^\*/
+        answer[:correct] = true
+      end
+      answer[:answer].sub!(/^\* */, '')
+      question[:answers] << answer
+    end
+  end
+  questions << question
+  questions
 end
 
 def run
